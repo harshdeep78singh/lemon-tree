b1025e7b28a778d2496d5381fe13b5746f1d5d34
Harshdeep Singh <56462840+lucifer288@users.noreply.github.com>
2021-09-03 09:56:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.10'
+DESCRIPTION = 'Print name in stars in a line'
+LONG_DESCRIPTION = 'A package to Print name in stars in a line'
+
+# Setting up
+setup(
+    name="Stars",
+    version=VERSION,
+    author="Harshdeep (lucifer288)",
+    author_email="<harshdeep78singh@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'stars' , 'lemon', 'tree' , 'poison' , 'jungle' , 'big'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
